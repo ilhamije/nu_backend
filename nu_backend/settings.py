3ed6349134da36c9683d85de075c6026752d8bc4
Ilham Akbar <ilhamije@gmail.com>
2022-06-26 23:08:03 +0700

    Fixing ReDoc static files

--- nu_backend/settings.py
+++ nu_backend/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATIC_URL = '/static/'
+STATIC_URL = '/static_root/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
