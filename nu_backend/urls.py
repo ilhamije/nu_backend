1ca60c2a544d7b2fa282a68507d0a654f04b3f14
Ilham Akbar <ilhamije@gmail.com>
2022-06-26 23:28:34 +0700

    Fixing static files

--- nu_backend/urls.py
+++ nu_backend/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
@@ -20,7 +21,7 @@ from django.views.static import serve
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-import settings
+
 
 schema_view = get_schema_view(
    openapi.Info(
