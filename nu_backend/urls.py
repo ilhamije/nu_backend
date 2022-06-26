5fbdbfb1db71c629ac5035cabf07b8ecbba23ad9
Ilham Akbar <ilhamije@gmail.com>
2022-06-26 23:21:19 +0700

    Fixing static files

--- nu_backend/urls.py
+++ nu_backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -35,4 +37,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/lapak/', include('lapaks.urls')),
     path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('static/', serve,{'document_root': 'settings.STATIC_ROOT'}),
+
 ]
